28d459a1158fd5317fd6542a423864ba5877a4ae
Hector Martin <marcan@marcan.st>
2022-07-08 02:06:15 +0900

    HID: core: Handle HOST bus type when announcing devices
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -2245,6 +2245,9 @@ int hid_connect(struct hid_device *hdev, unsigned int connect_mask)
 	case BUS_SPI:
 		bus = "SPI";
 		break;
+	case BUS_HOST:
+		bus = "HOST";
+		break;
 	case BUS_VIRTUAL:
 		bus = "VIRTUAL";
 		break;
