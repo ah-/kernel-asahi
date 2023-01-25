a08eea5f10c908a23198e72980b5891b453e5445
Miguel Ojeda <ojeda@kernel.org>
2023-01-25 16:37:37 +0000

    arm64: rust: Enable Rust support for AArch64
    This commit provides the build flags for Rust for AArch64. The core Rust
    support already in the kernel does the rest.
    
    The Rust samples have been tested with this commit.
    
    [jcunliffe: Arm specific parts taken from Miguel's upstream tree]
    
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>
    Co-developed-by: Jamie Cunliffe <Jamie.Cunliffe@arm.com>
    Signed-off-by: Jamie Cunliffe <Jamie.Cunliffe@arm.com>
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
    Reviewed-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>

--- scripts/generate_rust_target.rs
+++ scripts/generate_rust_target.rs
@@ -148,7 +148,18 @@ fn main() {
     let mut ts = TargetSpec::new();
 
     // `llvm-target`s are taken from `scripts/Makefile.clang`.
-    if cfg.has("X86_64") {
+    if cfg.has("ARM64") {
+        ts.push("arch", "aarch64");
+        ts.push(
+            "data-layout",
+            "e-m:e-i8:8:32-i16:16:32-i64:64-i128:128-n32:64-S128",
+        );
+        ts.push("disable-redzone", true);
+        ts.push("features", "+strict-align,+neon,+fp-armv8");
+        ts.push("llvm-target", "aarch64-linux-gnu");
+        ts.push("max-atomic-width", 128);
+        ts.push("target-pointer-width", "64");
+    } else if cfg.has("X86_64") {
         ts.push("arch", "x86_64");
         ts.push(
             "data-layout",
