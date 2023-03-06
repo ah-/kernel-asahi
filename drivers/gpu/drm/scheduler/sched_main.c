7e6e3175c03d181f36aefaddb0105adce8f8e95b
Asahi Lina <lina@asahilina.net>
2023-03-06 19:57:22 +0900

    drm/scheduler: Clean up jobs when the scheduler is torn down.
    drm_sched_fini() currently leaves any pending jobs dangling, which
    causes segfaults and other badness when job completion fences are
    signaled after the scheduler is torn down.
    
    Explicitly detach all jobs from their completion callbacks and free
    them. This makes it possible to write a sensible safe abstraction for
    drm_sched, without having to externally duplicate the tracking of
    in-flight jobs.
    
    This shouldn't regress any existing drivers, since calling
    drm_sched_fini() with any pending jobs is broken and this change should
    be a no-op if there are no pending jobs.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -1133,10 +1133,38 @@ EXPORT_SYMBOL(drm_sched_init);
 void drm_sched_fini(struct drm_gpu_scheduler *sched)
 {
 	struct drm_sched_entity *s_entity;
+	struct drm_sched_job *s_job, *tmp;
 	int i;
 
-	if (sched->thread)
-		kthread_stop(sched->thread);
+	if (!sched->thread)
+		return;
+
+	/*
+	 * Stop the scheduler, detaching all jobs from their hardware callbacks
+	 * and cleaning up complete jobs.
+	 */
+	drm_sched_stop(sched, NULL);
+
+	/*
+	 * Iterate through the pending job list and free all jobs.
+	 * This assumes the driver has either guaranteed jobs are already stopped, or that
+	 * otherwise it is responsible for keeping any necessary data structures for
+	 * in-progress jobs alive even when the free_job() callback is called early (e.g. by
+	 * putting them in its own queue or doing its own refcounting).
+	 */
+	list_for_each_entry_safe(s_job, tmp, &sched->pending_list, list) {
+		spin_lock(&sched->job_list_lock);
+		list_del_init(&s_job->list);
+		spin_unlock(&sched->job_list_lock);
+
+		dma_fence_set_error(&s_job->s_fence->finished, -ESRCH);
+		drm_sched_fence_finished(s_job->s_fence);
+
+		WARN_ON(s_job->s_fence->parent);
+		sched->ops->free_job(s_job);
+	}
+
+	kthread_stop(sched->thread);
 
 	for (i = DRM_SCHED_PRIORITY_COUNT - 1; i >= DRM_SCHED_PRIORITY_MIN; i--) {
 		struct drm_sched_rq *rq = &sched->sched_rq[i];
