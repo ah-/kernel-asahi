ffbb338cda67a8de3c70a72ce0ac9084c3251ea6
Hector Martin <marcan@marcan.st>
2023-04-16 21:29:03 +0900

    wifi: brcmfmac: Demote vendor-specific attach/detach messages to info
    People are getting spooked by brcmfmac errors on their boot console.
    There's no reason for these messages to be errors.
    
    Cc: stable@vger.kernel.org
    Fixes: d6a5c562214f ("wifi: brcmfmac: add support for vendor-specific firmware api")
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/bca/core.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/bca/core.c
@@ -12,13 +12,13 @@
 
 static int brcmf_bca_attach(struct brcmf_pub *drvr)
 {
-	pr_err("%s: executing\n", __func__);
+	pr_info("%s: executing\n", __func__);
 	return 0;
 }
 
 static void brcmf_bca_detach(struct brcmf_pub *drvr)
 {
-	pr_err("%s: executing\n", __func__);
+	pr_info("%s: executing\n", __func__);
 }
 
 const struct brcmf_fwvid_ops brcmf_bca_ops = {
